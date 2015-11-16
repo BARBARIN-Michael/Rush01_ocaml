2962666eb7bb162a48417b03ffa12b5409a24e20
mbarbari <mbarbari@student.42.fr>
2015-11-16 15:30:17 +0100

    Plein de truc...

---
+++ Graphics.ml
@@ -0,0 +1,62 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   Graphics.ml                                        :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: mbarbari <marvin@42.fr>                    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/11/16 14:07:32 by mbarbari          #+#    #+#             *)
+(*   Updated: 2015/11/16 15:29:09 by mbarbari         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+class virtual graphics_object screen (pos_x:int) (pos_y:int) (height:int) (width:int) =
+    object (self)
+        val _screen = screen
+        val _pos_x = pos_x
+        val _pos_y = pos_y
+        val _height = height
+        val _width = width
+
+        (* GETTEUR ********************************************************** *)
+        method getscreen = _screen
+        method getposx = _pos_x
+        method getposy = _pos_y
+        method getheight = _height
+        method getwidth = _width
+
+        (* VIRTUAL METHOD *************************************************** *)
+        method virtual action : unit
+
+        (* PUBLIC METHOD  *************************************************** *)
+        method draw (img: string) =
+            let img_load = Sdlloader.load_image img in
+            let pos_img = Sdlvideo.rect
+                    self#getposx self#getposy self#getheight self#getwidth in
+            Sdlvideo.blit_surface ~dst_rect:pos_img ~src:img_load ~dst:getscreen ();
+            Sdlvideo.flip getscreen; ()
+
+        method draw_text (str:string) =
+            if ((String.compare str "") <> 0) then
+                begin
+                    let font = Sdlttf.open_font "FontAAjouter.ttf" 24 in
+                    let text = Sdlttf.render_text_blended font str ~fg:Sdlvideo.white in
+                    let pos_text = Sdlvideo.rect
+                            (self#getposx + (self#getwidth / 3))
+                            (self#getposy + (self#getheight / 2))
+                            (self#getwidth / 2)
+                            (self#getheight / 2) in
+                    Sdlvideo.blit_surface ~dst_rect:pos_img ~src:img_load ~dst:getscreen ();
+                    Sdlvideo.flip getscreen; ()
+                end
+    end
+
+class button_eat (objtama: Tama.tama) (pos_x:int) (pos_y:int) (height:int) (width:int) =
+    object
+        inherit graphics_object pos_x pos_y height width
+
+        method action =
+            objtama = objtama#eat
+    end
+
+
--- Timer.ml
+++ Timer.ml
@@ -6,11 +6,11 @@
 (*   By: mbarbari <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/11/16 11:11:45 by mbarbari          #+#    #+#             *)
-(*   Updated: 2015/11/16 12:47:07 by mbarbari         ###   ########.fr       *)
+(*   Updated: 2015/11/16 14:04:35 by mbarbari         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-type t_coord = (x, y)
+type t_coord = x * y
 
 let rec timer_loop ((flag:bool), callback) (timing:float) =
     if !flag then Thread.exit
@@ -21,10 +21,16 @@ let handle_event (handle_object:UI_object) =
     if Sdlevent.has_event () then
         match Sdlevent.wait_event () with
             |Sdlevent.MOUSEBUTTONDOWN ->
-                    handle_graphics handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 1
+                    handle_mouse handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 1
             |Sdlevent.MOUSEBUTTONUP ->
-                    handle_graphics handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 2
+                    handle_mouse handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 2
+            |Sdlevent.KEYDOWN ->
+                    handle_key ()
 
-let handle_graphics (handle_object: UI_object) (x, v) action = match handle_object with
+let handle_mouse (handle_object: UIobject) (x, v) action = match handle_object with
     |a::b when (a.coord_inner_object x y) = true && action = 1 -> a.action; ()
     |[] -> ()
+
+let handle_key = match Sdlevent.wait_event () with
+    |Sdlevent.KEYDOWN {keysym=KEY_ESCAPE} -> exit 0; ()
+    |_ -> ()
