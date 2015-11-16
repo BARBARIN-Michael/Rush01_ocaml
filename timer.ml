5e1c9934c871a6b33b69251ea09a3bb147af4617
mbarbari <mbarbari@debian>
2015-11-16 12:48:08 +0100

    petit idee de depart pour le graphics, creer les element en une liste d'objet qui proviens d'une class virtuel

--- timer.ml
+++ timer.ml
@@ -6,19 +6,25 @@
 (*   By: mbarbari <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/11/16 11:11:45 by mbarbari          #+#    #+#             *)
-(*   Updated: 2015/11/16 11:42:40 by mbarbari         ###   ########.fr       *)
+(*   Updated: 2015/11/16 12:47:07 by mbarbari         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
+
+type t_coord = (x, y)
 
 let rec timer_loop ((flag:bool), callback) (timing:float) =
     if !flag then Thread.exit
     else
         (Thread.delay timing; callback; timer_loop(flag, callback))
 
-let rec handle_event =
+let handle_event (handle_object:UI_object) =
     if Sdlevent.has_event () then
         match Sdlevent.wait_event () with
             |Sdlevent.MOUSEBUTTONDOWN ->
-                    handle_graphic Sdlevent.mbe_x Sdlevent.mbe_y 1
+                    handle_graphics handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 1
             |Sdlevent.MOUSEBUTTONUP ->
-                    handle_graphic Sdlevent.mbe
+                    handle_graphics handle_object (Sdlevent.mbe_x, Sdlevent.mbe_y) 2
+
+let handle_graphics (handle_object: UI_object) (x, v) action = match handle_object with
+    |a::b when (a.coord_inner_object x y) = true && action = 1 -> a.action; ()
+    |[] -> ()
